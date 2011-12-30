9e4fdeba929023cb716c14bbe9b1bab5c4daecad
Peter Sobot <psobot@gmail.com>
2011-12-29 21:00:44 -0500

    Fuck RVM.

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,11 +22,6 @@ task :production do
   role :web,  domain
   role :db,   domain, :primary => true
   set :whenever_environment, 'production'
-
-  set :rvm_type, :user 
-  $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
-  require "rvm/capistrano"                  # Load RVM's capistrano plugin.
-  set :rvm_ruby_string, '1.9.2'        # Or whatever env you want it to run in.
 end
 
 set :whenever_command, 'bundle exec whenever'
