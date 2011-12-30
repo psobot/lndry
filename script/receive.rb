fa76b274ac0ab285c93ad6f93dbe4e2bfb187b7e
Peter Sobot <psobot@gmail.com>
2011-12-30 18:40:35 -0500

    Fixed bug with double file reading.

--- script/receive.rb
+++ script/receive.rb
@@ -16,14 +16,14 @@ HOST = settings[env]['host']
 INCOMING_EMAIL_KEY = settings[env]['incoming_email_key']
 
 if env == 'production'
-  email = STDIN
+  email = STDIN.read
 else
-  email = File.open(File.dirname(__FILE__) + '/mail.txt')
+  email = File.open(File.dirname(__FILE__) + '/mail.txt').read
 end
-message = Mail.read_from_string(email.read)
+message = Mail.read_from_string(email)
 
 name = message[:from].to_s.split('<')[0].strip rescue nil
 email_address = message[:from].addresses[0] rescue nil
 to = message[:to]
 
-`curl -d name=#{CGI.escape(name)} -d email=#{CGI.escape(email_address)} -d key=#{CGI.escape(INCOMING_EMAIL_KEY)} -d raw=#{CGI.escape(email.read)} -d to=#{CGI.escape(to.to_s)} http://#{HOST}/receive`
+`curl -d name=#{CGI.escape(name)} -d email=#{CGI.escape(email_address)} -d key=#{CGI.escape(INCOMING_EMAIL_KEY)} -d raw=#{CGI.escape(email)} -d to=#{CGI.escape(to.to_s)} http://#{HOST}/receive`
