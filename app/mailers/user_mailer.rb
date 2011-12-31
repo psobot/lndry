400d141ae501039f506829ec0a1dcea3ade57739
Peter Sobot <psobot@gmail.com>
2011-12-31 13:03:15 -0500

    Added name parsing and email variables.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,16 +1,14 @@
 class UserMailer < PostageApp::Mailer
-  default from: "mailer@lndry.com"
   default_url_options[:host] = defined?(HOST) ? HOST : 'localhost:3000'
 
   def done_reminder_for(user)
     postageapp_template 'done'
-    mail :to => { user.email => { :name => user.email } }, :subject => ''
+    mail :to => { user.email => user.email_variables }, :subject => ''
   end
 
   def available_reminder_for(user)
     postageapp_template 'available'
-    mail :to => { user.email => { :name => user.email } }, :subject => ''
+    mail :to => { user.email => user.email_variables }, :subject => ''
   end
-
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,30 @@ class User < ActiveRecord::Base
   has_many :uses
   has_many :messages
 
+  def first_name
+    pieces = name.split(' ')
+    if pieces.length == 1
+      pieces[0]
+    else
+      pieces[0...-1].join(' ')
+    end
+  rescue
+    nil
+  end
+
+  def last_name
+    pieces = name.split(' ')
+    pieces[-1]
+  rescue
+    nil
+  end
+
+  def email_variables
+    {
+      :first_name => first_name,
+      :last_name => last_name,
+      :name => name
+    }
+  end
+
 end
