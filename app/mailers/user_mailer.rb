f96c16ae31b5532fe79e29211141de20a4c440cd
Peter Sobot <psobot@gmail.com>
2012-01-13 02:53:13 -0500

    Whoops, broke things with commit. Stupid me.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,12 +3,12 @@ class UserMailer < PostageApp::Mailer
 
   def done_reminder_for(user, use)
     postageapp_template 'done'
-    mail :to => { user.email => user.email_variables.merge(use.email_variables) }, :subject => ''
+    mail :to => { user.email => user.email_variables.merge(use ? use.email_variables : {}) }, :subject => ''
   end
 
   def available_reminder_for(user, resource)
     postageapp_template 'available'
-    mail :to => { user.email => user.email_variables.merge(resource.email_variables) }, :subject => ''
+    mail :to => { user.email => user.email_variables.merge(resource ? resource.email_variables : {}) }, :subject => ''
   end
 
 end
