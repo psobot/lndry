70da7079f8069fbf4332c40d49ac0e610e05f0ed
Peter Sobot <psobot@gmail.com>
2012-05-05 00:54:00 -0700

    Added proper output of raw email in admin panel.

--- app/admin/uses.rb
+++ app/admin/uses.rb
@@ -1,5 +1,25 @@
 ActiveAdmin.register Use do
   actions :index, :show
+  
+  show do |use|
+    attributes_table do
+      row :id
+      row :user
+      row :resource
+      row :start
+      row :finish
+      row(:raw_email) do |use|
+        if use.raw_email.empty?
+          span( class: "empty" ) {"Empty"}
+        else
+          pre(use.raw_email)
+        end
+      end
+      row :created_at
+      row :updated_at
+    end
+  end
+
   index do
     column :id
     column :resource
