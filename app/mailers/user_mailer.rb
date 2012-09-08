4b3cee99869d861ee79e499646c8b98628645e65
Peter Sobot <psobot@gmail.com>
2012-09-08 17:31:05 -0400

    Fixed failed PostageApp request handling and resend behaviour. Added timestamps to avoid message deduplication.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,26 @@
 class UserMailer < PostageApp::Mailer
   default_url_options[:host] = defined?(HOST) ? HOST : 'localhost:3000'
 
+  def default_vars
+    {:timestamp => Time.now.to_i}
+  end
+
   def done_reminder_for(user, use)
     postageapp_template 'done'
-    mail :to => { user.email => user.email_variables.merge(use ? use.email_variables : {}) }, :subject => ''
+    mail :to => {
+      user.email => user.email_variables.
+                    merge(use ? use.email_variables : {}).
+                    merge(default_vars)
+    }, :subject => ''
   end
 
   def available_reminder_for(user, resource)
     postageapp_template 'available'
-    mail :to => { user.email => user.email_variables.merge(resource ? resource.email_variables : {}) }, :subject => ''
+    mail :to => {
+      user.email => user.email_variables.
+                    merge(resource ? resource.email_variables : {}).
+                    merge(default_vars)
+    }, :subject => ''
   end
 
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -4,26 +4,26 @@ class Message < ActiveRecord::Base
   belongs_to :use
 
   def self.send_all
-    logger.debug "Sending messages to everybody..."
+    logger.debug "Resending failed requests..."
+    PostageApp::FailedRequest.resend_all
+    logger.debug "Evaluating and sending messages..."
     all.each do |message|
       logger.debug "Evaluating message for user ##{message.user_id}"
       if message.when and message.when < Time.now.utc
         response = UserMailer.done_reminder_for(message.user, message.use).deliver #TODO: get reference to use
-        if response.status == "ok"
-          message.destroy
-        else
-          logger.debug "Whoops, something went very wrong. #{response.message}"
+        if response.status != "ok"
+          logger.warning "Could not immediately send message: #{response.message}"
         end
+        message.destroy
       elsif message.if
         begin
           valid = Resource.send(message.if)
           if valid
             response = UserMailer.available_reminder_for(message.user, valid).deliver  #TODO: get reference to resource
-            if response.status == "ok"
-              message.destroy
-            else
-              logger.debug "Whoops, something went very wrong. #{response.message}"
+            if response.status != "ok"
+              logger.warning "Could not immediately send message: #{response.message}"
             end
+            message.destroy
           end
         rescue NoMethodError
           logger.debug "NoMethodError on Resource for method: \"#{message.if}\""
