7d50fd83ed8d7a6f5fcbae0e4ba73c392f5f1945
Peter Sobot <psobot@gmail.com>
2013-11-23 13:54:59 -0500

    Added support for Heroku-based initializers.

--- config/initializers/lndry.rb
+++ config/initializers/lndry.rb
@@ -1,11 +1,23 @@
-settings_file = File.expand_path('../settings.yml', File.dirname(__FILE__))
-settings = YAML::load(File.open(settings_file))
-env = Rails.env
+begin
+    settings_file = File.expand_path('../settings.yml', File.dirname(__FILE__))
+    settings = YAML::load(File.open(settings_file))
+    env = Rails.env
 
-PostageApp.configure do |config|
-  config.api_key = settings[env]['postageapp_api_key']
-  config.recipient_override = settings[env]['recipient_override']
+    PostageApp.configure do |config|
+      config.api_key = settings[env]['postageapp_api_key']
+      config.recipient_override = settings[env]['recipient_override']
+    end
+
+    HOST = settings[env]['host']
+    INCOMING_EMAIL_KEY = settings[env]['incoming_email_key']
+rescue Errno::ENOENT
+    env = Rails.env
+
+    PostageApp.configure do |config|
+      config.api_key = ENV['postageapp_api_key']
+      config.recipient_override = ENV['recipient_override']
+    end
+
+    HOST = ENV['host']
+    INCOMING_EMAIL_KEY = ENV['incoming_email_key']
 end
-
-HOST = settings[env]['host']
-INCOMING_EMAIL_KEY = settings[env]['incoming_email_key']
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,7 +4,11 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-settings_file = File.expand_path('../settings.yml', File.dirname(__FILE__))
-settings = YAML::load(File.open(settings_file))
-env = Rails.env
-Lndry::Application.config.secret_token = settings[env]['secret_token']
+begin
+    settings_file = File.expand_path('../settings.yml', File.dirname(__FILE__))
+    settings = YAML::load(File.open(settings_file))
+    env = Rails.env
+    Lndry::Application.config.secret_token = settings[env]['secret_token']
+rescue Errno::ENOENT
+    Lndry::Application.config.secret_token = ENV['secret_token']
+end
